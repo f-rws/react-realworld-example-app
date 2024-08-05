19f9b2c3f2f96ae14cf643ccd71e0b8ed6f9ab01
Hiroya Furusawa <pogbba.6@gmail.com>
2024-08-06 01:58:14 +0900

    feat: TanStack Routerを導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router;
+    }
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 );
