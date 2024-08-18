8da1edab7ee2fe782251e192c0dc40cd87f939a6
Hiroya Furusawa <pogbba.6@gmail.com>
2024-08-18 20:31:28 +0900

    feat: テンプレートを追加

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -5,5 +5,48 @@ export const Route = createFileRoute("/register")({
 });
 
 function Register() {
-    return <div>register</div>;
+    return (
+        <div className="auth-page">
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 col-xs-12">
+                        <h1 className="text-xs-center">Sign up</h1>
+                        <p className="text-xs-center">
+                            <a href="/login">Have an account?</a>
+                        </p>
+
+                        {/* TODO: APIレスポンスのエラーメッセージを表示させる */}
+                        {/*<ul className="error-messages">*/}
+                        {/*    <li>That email is already taken</li>*/}
+                        {/*</ul>*/}
+
+                        <form>
+                            <fieldset className="form-group">
+                                <input
+                                    className="form-control form-control-lg"
+                                    type="text"
+                                    placeholder="Username"
+                                />
+                            </fieldset>
+                            <fieldset className="form-group">
+                                <input
+                                    className="form-control form-control-lg"
+                                    type="text"
+                                    placeholder="Email"
+                                />
+                            </fieldset>
+                            <fieldset className="form-group">
+                                <input
+                                    className="form-control form-control-lg"
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            </fieldset>
+                            <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
