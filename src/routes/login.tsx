9a4be2f26dac056f5c30f757ffd4023afc9fafd5
Hiroya Furusawa <pogbba.6@gmail.com>
2024-08-21 00:45:04 +0900

    refactor: Linkコンポーネントに置き換え

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -34,7 +34,7 @@ function Login() {
                     <div className="col-md-6 offset-md-3 col-xs-12">
                         <h1 className="text-xs-center">Sign in</h1>
                         <p className="text-xs-center">
-                            <a href="/register">Need an account?</a>
+                            <Link to={"/register"}>Need an account?</Link>
                         </p>
 
                         {/* TODO: APIレスポンスのエラーメッセージを表示させる */}
