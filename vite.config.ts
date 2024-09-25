e3746d72f298eddbcd1cd87d86e1daf95afbfd82
Hiroya Furusawa <pogbba.6@gmail.com>
2024-09-26 01:53:42 +0900

    chore: テスト関連のAPIをグローバルで使用できるように変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [TanStackRouterVite(), react(), viteTsconfigPaths()],
+    test: {
+        globals: true,
+    },
 });
