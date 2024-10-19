23dc9913d4a99b24f4173c271520675c8740901e
Hiroya Furusawa <pogbba.6@gmail.com>
2024-10-19 18:53:04 +0900

    feat: UIカタログを追加

---
+++ .ladle/config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('@ladle/react').UserConfig} */
+export default {
+    appendToHead:
+        `<style>
+            .ladle-wrapper { flex-direction: row-reverse; }
+        </style>`,
+}
---
+++ src/exaple-story/index.stories.tsx
@@ -0,0 +1,3 @@
+import type { Story } from "@ladle/react";
+
+export const ExampleButton: Story = () => <button>test button</button>;
